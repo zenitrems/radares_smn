4a9d78509af44710a526f16b1863a73ff4678f82
Fernando Martínez <46614852+zenitrems@users.noreply.github.com>
2025-07-07 00:27:40 -0500

    Add Next.js Leaflet map

---
+++ web/src/Map.js
@@ -0,0 +1,23 @@
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+
+export default function Map({ radars }) {
+  const position = [23.6345, -102.5528]; // Center of Mexico
+
+  return (
+    <MapContainer center={position} zoom={5} style={{ height: '100%', width: '100%' }}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {radars.filter(r => r.show).map(radar => (
+        <Marker key={radar.urlName} position={radar.markerCenter} icon={L.icon({
+          iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+          shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
+        })}>
+          <Popup>{radar.showName}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
