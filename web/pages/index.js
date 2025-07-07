ca00f23bf1c1df4791fa77994e69357575c0806a
zenitrems <46614852+zenitrems@users.noreply.github.com>
2025-07-07 02:28:05 -0500

    Cargar gifs en slider

--- web/pages/index.js
+++ web/pages/index.js
@@ -3,11 +3,15 @@ import radars from '../radares.json';
 import 'leaflet/dist/leaflet.css';
 
 const Map = dynamic(() => import('../src/Map'), { ssr: false });
+const GifSlider = dynamic(() => import('../src/GifSlider'), { ssr: false });
 
 export default function Home() {
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <Map radars={radars} />
+    <div>
+      <div style={{ height: '60vh', width: '100%' }}>
+        <Map radars={radars} />
+      </div>
+      <GifSlider />
     </div>
   );
 }
