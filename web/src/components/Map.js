d9875b1ff7a5e7fdb128aedcce3fe0f28d6f3b8b
zenitrems <46614852+zenitrems@users.noreply.github.com>
2025-07-09 01:36:57 -0500

    Add Map to components

--- web/src/components/Map.js
+++ web/src/components/Map.js
@@ -2,10 +2,12 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 
 export default function Map({ radars }) {
-  const position = [23.6345, -102.5528]; // Center of Mexico
+  /* const position = [23.6345, -102.5528]; // Center of Mexico */
+  const position = [21.028969, -86.852031]; // Center of radar cancun
+  
 
   return (
-    <MapContainer center={position} zoom={5} style={{ height: '100%', width: '100%' }}>
+    <MapContainer center={position} zoom={9} style={{ height: '100%', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
