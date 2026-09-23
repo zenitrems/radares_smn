c7a42babb94a7e137228b7ebc9c595c961dec850
zenitrems <46614852+zenitrems@users.noreply.github.com>
2026-09-22 23:21:52 -0500

    Rol solo lectura consola web y definicion en .env

--- db/roles.sql
+++ db/roles.sql
@@ -2,5 +2,11 @@
 
 CREATE ROLE ingesta_estaciones_smn LOGIN PASSWORD 'cambiar';
 
-GRANT CONNECT ON DATABASE smn TO ingesta_estaciones_smn;
+GRANT CONNECT ON DATABASE estaciones_smn TO ingesta_estaciones_smn;
 GRANT SELECT, INSERT, UPDATE ON estaciones, observaciones TO ingesta_estaciones_smn;
+
+-- Rol de sólo lectura para la consola web.
+CREATE ROLE web_estaciones_smn LOGIN PASSWORD 'cambiar';
+
+GRANT CONNECT ON DATABASE estaciones_smn TO web_estaciones_smn;
+GRANT SELECT ON estaciones, observaciones, ultima_observacion TO web_estaciones_smn;
